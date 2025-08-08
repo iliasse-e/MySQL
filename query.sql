772eeb63b7ed8c18ab632e34aab515b3e468aa25
Iliasse <61607450+iliasse-e@users.noreply.github.com>
2025-08-08 12:26:19 +0200

    Update query.sql

--- query.sql
+++ query.sql
@@ -13,3 +13,13 @@ SELECT title, popularity FROM movie WHERE budget < 2000000; --
 SELECT nom, ville FROM utilisateurs WHERE age >= 18 AND ville = 'Paris'; -- using AND, OR, NOT
 
 SELECT * FROM produits WHERE prix BETWEEN 10 AND 20; -- BETWEEN min AND max
+
+SELECT * FROM employes WHERE (departement = 'Ventes' OR departement = 'IT') AND salaire > 3000; -- Using () is crucial to respect an order
+
+SELECT * FROM movie WHERE title LIKE 'a%'; -- Starts with 'a'
+
+SELECT * FROM movie WHERE title LIKE '%a'; -- End with 'a'
+
+SELECT * FROM movie WHERE title LIKE '%a%' -- Contains 
+
+SELECT * FROM movie WHERE title LIKE 'a%s' -- Starts with 'a' ends with 's'
